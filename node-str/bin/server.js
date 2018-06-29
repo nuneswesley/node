116ea344df3bae55b9e52f6757c4483e2297d028
nuneswesley <nunes.wesley@hotmail.com>
2018-06-28 22:26:00 -0300

    crud REST

--- node-str/bin/server.js
+++ node-str/bin/server.js
@@ -1,27 +1,15 @@
-'use strict'
-
 const http = require('http');
-const debug = require('debug')('nodestr:server')
-const express = require('express')
+const debug = require('debug')('nodestr:server');
+const app = require('../src/app');
 
-const app = express();
 const port = normalizePort(process.env.port || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
 
 console.log('API rodando na porta ' + port);
 
@@ -60,4 +48,12 @@ function onError(error) {
         default:
             throw error;
     }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
 }
