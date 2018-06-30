3ddbbb9030d2c6cecc10fcd7c8f1a858207d6dcf
nuneswesley <nunes.wesley@hotmail.com>
2018-06-30 00:03:55 -0300

    add mogoose

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -2,16 +2,22 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+const router = express.Router();
+
+// Connecta ao banco
+mongoose.connect('mongodb://wesley:123Aa321@ds018258.mlab.com:18258/nodestore')
+
+// carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const indexRoute = require('./routes/index-route');
-const indexProduct = require('./routes/product-route');
-
 app.use('/', indexRoute);
-app.use('/products', indexProduct);
+app.use('/products', productRoute);
 
 module.exports = app;
